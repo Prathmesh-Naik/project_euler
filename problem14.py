ceaac77268537be40e8167df5a72a069d81e224a
P N <prathmeshnaik.1202@gmail.com>
2020-12-31 09:54:53 +0530

    -

---
+++ problem14.py
@@ -0,0 +1,19 @@
+max_count = [0,0]
+i = 13
+
+while i <= 1000000:
+    count = 0
+    temp = i
+    while temp != 1:
+        if temp%2 ==0:
+            temp /= 2
+        else:
+            temp = (3*temp) + 1
+        count += 1
+
+    if count > max_count[0]:
+        max_count = [count, i]
+
+    i += 1
+
+print(max_count)
